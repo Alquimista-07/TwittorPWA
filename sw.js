e357134991ffba626768c5fc11da2e6ee70daf26
Alquimista-07 <jdca.07@gmail.com>
2022-03-13 11:51:37 -0500

    Mejorando la apariencia en iOS

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 // Creamos unas constantes con los tipos de cache que vamos a manejar
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v2';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
